547b808717cbabf0ad7dbb91c80ec1ffc17e68cd
mtugb <main@mtugb.com>
2026-01-16 15:48:11 +0900

    docs:README.mdに説明を追加しました。

--- build.rs
+++ build.rs
@@ -1,9 +1,7 @@
-// build.rs
 fn main() {
     if std::env::var("CARGO_CFG_WINDOWS").is_ok() {
         let mut res = winres::WindowsResource::new();
-        // ここにアイコンファイル（.ico）のパスを指定
-        res.set_icon("/icon.ico");
+        res.set_icon("icon.ico");
         res.compile().unwrap();
     }
 }
